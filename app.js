4f12547650f99d2dfc777078f62ca5492219450e
spencerasj <spe19021@byui.edu>
2021-10-07 16:16:55 -0600

    trying to get mongodb and heroku to be friends again

--- app.js
+++ app.js
@@ -35,6 +35,23 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+const corsOptions = {
+  origin: "https://cse341-prove-heroku.herokuapp.com/",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4,
+};
+
+const MONGODB_URL =
+  process.env.MONGODB_URL ||
+  "mongodb+srv://Jen:fifl6xklo2dFi0w8@cluster0.b2hr6.mongodb.net/shop?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGODB_URL, options)
@@ -59,20 +76,3 @@ mongoose
 
 // const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 // ...
-const corsOptions = {
-  origin: "https://cse341-prove-heroku.herokuapp.com/",
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4,
-};
-
-const MONGODB_URL =
-  process.env.MONGODB_URL ||
-  "mongodb+srv://Jen:fifl6xklo2dFi0w8@cluster0.b2hr6.mongodb.net/shop?retryWrites=true&w=majority";
